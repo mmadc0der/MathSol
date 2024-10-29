516cd5a516d1d1344b524207966b361c2cabfc6a
mmadc0der <tim110702@mail.ru>
2024-10-30 00:34:23 +0300

    feat: + LiteralExpression

--- src/parser/include/expression.hpp
+++ src/parser/include/expression.hpp
@@ -11,14 +11,24 @@ class IExpression {
     T virtual evaluate() const = 0;
 };
 
-template<typename T, typename L, typename R>
+template<typename T>
+class LiteralExpression : public IExpression {
+  public:
+    LiteralExpression(Literal<T>& literal_) : literal_(literal_) {};
+    T evaluate() const override;
+  
+  private:
+    Literal<T> literal_;
+};
+
+template<typename T, typename L>
 class BinaryExpression : public IExpression<T> {
   public:
-    BinaryExpression(Literal<T>& left, Literal<T>& right, Token& operator_) : left_(left), right_(right), operator_(operator_) {};
+    BinaryExpression(Literal<T>* left, Literal<T>* right, const Token& operator_) : left_(left), right_(right), operator_(operator_) {};
     T evaluate() override;
 
   private:
-    Literal<L> left_;
-    Literal<R> right_;
+    IExpression<L>* left_;
+    IExpression<L>* right_;
     Token operator_;
 };
